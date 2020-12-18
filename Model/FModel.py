b9fccd03f412ca79f3313c4737d673d8b566353e
MenggeLiu <lmg864355282@gmail.com>
2020-12-18 11:25:10 +0800

    ready to submit homework

--- Model/FModel.py
+++ Model/FModel.py
@@ -13,10 +13,10 @@ class FModel(nn.Module, ABC):
     def __init__(self, d_in, d_hid, d_class, n_layers, bi=True, dropout=0.3):
         super().__init__()
         # self.model_path = r'C:\Users\86435\Documents\work_pycharm\work_NER\chinese-xlnet-mid'
-        # self.model_path = r'/data/lingvo_data/transformers_model/chinese-xlnet-mid'
-        self.model_path = r'/data/mgliu/transformers_model/roberta_chinese_clue_large'
-        # self.model = BertModel.from_pretrained("clue/roberta_chinese_clue_large")
-        self.model = BertModel.from_pretrained(self.model_path)
+        # self.model_path = r'/data/mgliu/transformers_model/chinese-xlnet-mid'
+        # self.model_path = r'/data/mgliu/transformers_model/roberta_chinese_clue_large'
+        self.model = BertModel.from_pretrained("clue/roberta_chinese_clue_large")
+        # self.model = BertModel.from_pretrained(self.model_path)
         # self.model = XLNetModel.from_pretrained("hfl/chinese-xlnet-mid", mem_len=1024)
         self.bilstm = nn.LSTM(d_in, 200, num_layers=3, batch_first=True, dropout=0.4,
                               bidirectional=bi)
